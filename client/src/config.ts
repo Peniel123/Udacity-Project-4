d5bad63a701bbb09e8c0a898ba75727facb6bb07
Peniel Ilechie <Peniel.Ilechie@konga.local>
2022-08-31 10:15:20 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'rz73y3vm8g';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/main`
+
+export const authConfig = {
+  domain: 'dev-g1whpvzh.us.auth0.com',
+  clientId: 'SHzzJI1TSQR4XX4UcYD599m39e0IcioO',
+  callbackUrl: 'http://localhost:3000/callback'
+};
